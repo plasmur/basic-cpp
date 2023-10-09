e54cd83871a33c467847e602b78038084c0e50f7
bimba Muhammad andra putra hendriyanto <bimba031006@gmail.com>
2023-10-09 19:31:52 +0700

    metode-output-input

---
+++ logic-gates/ifclass.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int grade;
+  string name;
+  string nis;
+
+  cout << "enter your name: ";
+  cin >> name;
+
+  cout << " \nIn which grade do you study (7-12):";
+  cin >> grade;
+
+  cout << "\nEnter your NIS:";
+  cin >> nis;
+}
